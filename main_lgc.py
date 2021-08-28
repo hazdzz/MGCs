64c8ca58345159811a512c3d2937bc47377dd3a4
hazdzz <raphaelpeo@gmail.com>
2021-08-29 05:40:13 +0800

    add results

--- main_lgc.py
+++ main_lgc.py
@@ -39,7 +39,7 @@ def get_parameters():
                         help='the path of dataset config file, cora.ini for CoRA')
     parser.add_argument('--model_config_path', type=str, default='./config/model/lgc_sym.ini', \
                         help='the path of model config file')
-    parser.add_argument('--gamma', type=float, default=2, help='gamma >= 2, defaut as 2')
+    parser.add_argument('--gamma', type=float, default=1, help='gamma >= 2, defaut as 2')
     parser.add_argument('--enable_bias', type=bool, default=True, help='enable to use bias in graph convolution layers or not')
     parser.add_argument('--epochs', type=int, default=10000, help='epochs, default as 10000')
     parser.add_argument('--opt', type=str, default='Adam', help='optimizer, default as Adam')
@@ -98,7 +98,7 @@ def get_parameters():
         param = nni.get_next_parameter()
         gamma = [*param.values()][0]
     else:
-        if args.gamma < 2:
+        if args.gamma < 1:
             raise ValueError(f'ERROR: The value of gamma is unacceptable.')
         else:
             gamma = args.gamma
